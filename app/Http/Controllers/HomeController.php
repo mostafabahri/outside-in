1de77287df369bad6c4cd7af78c53116d60902d3
Mostafa Bahri <mostafa@turpal.com>
2021-09-24 13:46:12 +0330

    bind app data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,17 +9,7 @@ class HomeController extends Controller
 {
     public function __invoke(IProductService $productService)
     {
-        // new FeaturedProductViewModel([
-        //                 new ProductViewModel('Criollo Chocolate', 39.45),
-        //                 new ProductViewModel('Gruyere', 48.50),
-        //                 new ProductViewModel('White Asparguras', 29.99),
-        //                 new ProductViewModel('Anchovoris', 19.99),
-        //                 new ProductViewModel('Arborio Rice', 22.75)
-        //             ])
-        // ];
-
         $model = $productService->getFeaturedProducts()
-            // ->mapInto(ProductViewModel::class)
             ->pipeInto(FeaturedProductViewModel::class);
 
         return view('home', ['model' => $model]);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Domain\IProductService;
+use App\Http\ViewModels\ProductViewModel;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +17,22 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind(
+            IProductService::class,
+            fn () => new class  implements IProductService
+            {
+                public function getFeaturedProducts(): Collection
+                {
+                    return collect([
+                        new ProductViewModel('Criollo Chocolate', 39.45),
+                        new ProductViewModel('Gruyere', 48.50),
+                        new ProductViewModel('White Asparguras', 29.99),
+                        new ProductViewModel('Anchovoris', 19.99),
+                        new ProductViewModel('Arborio Rice', 22.75)
+                    ]);
+                }
+            }
+        );
     }
 
     /**
