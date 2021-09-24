273b758a0d0165ab917457595c4944331a3e63a3
Mostafa Bahri <mostafa@turpal.com>
2021-09-24 11:37:00 +0330

    change w/ cooking data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,32 +15,23 @@
         <h1>Featured Products</h1>
         <p>This is a demo app for showcasing the outside-in approach using DI concepts.</p>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Godzilla</td>
-                    <td>$299.99</td>
-                </tr>
-                <tr>
-                    <td>Mozilla</td>
-                    <td>$100,000.00</td>
-                </tr>
-                <tr>
-                    <td>Quesadilla</td>
-                    <td>$2.22</td>
-                </tr>
-                <tr>
-                    <td>Fakezilla</td>
-                    <td>$19.99</td>
-                </tr>
-            </tbody>
-        </table>
+        <ul>
+            <li>
+                Criollo Chocolate ($39.45)
+            </li>
+            <li>
+                Gruyere ($48.50)
+            </li>
+            <li>
+                White Asparguras ($29.99)
+            </li>
+            <li>
+                Anchovoris ($19.99)
+            </li>
+            <li>
+                Arborio Rice ($22.75)
+            </li>
+        </ul>
     </main>
 
 </body>
