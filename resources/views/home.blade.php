682dd97835485229439d5748b9acf423deeda410
Mostafa Bahri <mostafa@turpal.com>
2021-09-24 11:22:44 +0330

    add hard-coded home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Outside in</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+
+<body>
+
+    <main>
+        <h1>Featured Products</h1>
+        <p>This is a demo app for showcasing the outside-in approach using DI concepts.</p>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Godzilla</td>
+                    <td>$299.99</td>
+                </tr>
+                <tr>
+                    <td>Mozilla</td>
+                    <td>$100,000.00</td>
+                </tr>
+                <tr>
+                    <td>Quesadilla</td>
+                    <td>$2.22</td>
+                </tr>
+                <tr>
+                    <td>Fakezilla</td>
+                    <td>$19.99</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+
+</body>
+
+</html>
